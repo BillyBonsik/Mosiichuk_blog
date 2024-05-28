cacc4b547c0023e44bd26fb86a114af7ae4853d0
BillyBonsik <146487950+BillyBonsik@users.noreply.github.com>
2024-05-28 11:44:51 +0300

    feat: add 2-3 lab

--- Mosiichuk_laravel/blog/routes/web.php
+++ Mosiichuk_laravel/blog/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RestTestController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -11,7 +12,12 @@ Route::middleware([
     config('jetstream.auth_session'),
     'verified',
 ])->group(function () {
+
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
+});
+Route::resource('rest', RestTestController::class)->names('restTest');
+Route::group([ 'namespace' => 'App\Http\Controllers\Blog', 'prefix' => 'blog'], function () {
+    Route::resource('posts', PostController::class)->names('blog.posts');
 });
